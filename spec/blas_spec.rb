0971630bedb6b46a2fffc69787ce085df0576771
prasun <prasunanand.bitsp@gmail.com>
2017-05-31 14:00:42 +0530

    blas spec

---
+++ spec/blas_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe ArrayFire::BLAS do
+  context '#matmul' do
+    let(:a) { ArrayFire::Af_Array.new 2, [2,2],[1,2,3,4] }
+    let(:b) { ArrayFire::Af_Array.new 2, [2,2],[1,2,3,4] }
+    let(:c) { ArrayFire::Af_Array.new 2, [2,2],[7,10,15,22] }
+    subject {c}
+    it {expect(ArrayFire::BLAS.matmul(a,b)).to eq c}
+  end
+end
