f3bf1d9b9b0b9451a4eeaf377484d0b33dd875ff
prasun <prasunanand.bitsp@gmail.com>
2017-06-01 22:23:35 +0530

    init methods for Blas

--- spec/blas_spec.rb
+++ spec/blas_spec.rb
@@ -4,8 +4,8 @@ describe ArrayFire::BLAS do
   context '#matmul' do
     let(:a) { ArrayFire::Af_Array.new 2, [2,2],[1,2,3,4] }
     let(:b) { ArrayFire::Af_Array.new 2, [2,2],[1,2,3,4] }
-    let(:c) { ArrayFire::Af_Array.new 2, [2,2],[7,10,15,22] }
+    let(:c) { ArrayFire::Af_Array.new 2, [2,2],[7.0,10.0,15.0,22.0] }
     subject {c}
-    it {expect(ArrayFire::BLAS.matmul(a,b)).array to eq c.array}
+    it {expect(ArrayFire::BLAS.matmul(a,b).array).to eq c.array}
   end
 end
