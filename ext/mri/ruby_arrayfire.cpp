16fd9d06700d3b7611ccea505cb0747725bf0b86
Prasun Anand <prasunanand.bitsp@gmail.com>
2017-06-29 00:43:26 +0530

    more cleanup

--- ext/mri/ruby_arrayfire.cpp
+++ ext/mri/ruby_arrayfire.cpp
@@ -13,40 +13,6 @@
 
 namespace arf {
 
-  enum ewop_t {
-    EW_ADD,
-    EW_SUB,
-    EW_MUL,
-    EW_DIV,
-    EW_POW,
-    EW_MOD,
-    EW_EQEQ,
-    EW_NEQ,
-    EW_LT,
-    EW_GT,
-    EW_LEQ,
-    EW_GEQ,
-  };
-  using namespace af;
-  static array A; // populated before each timing
-  static void fn()
-  {
-      array B = matmul(A, A);  // matrix multiply
-      B.eval();                // ensure evaluated
-  }
-
-  static void createArray(afstruct *afarray)
-  {
-    dim_t dims[afarray->ndims] ;
-
-    for (size_t index = 0; index < afarray->ndims; ++index){
-      dims[index] = (dim_t)afarray->dimension[index];
-    }
-  }
-
-  #include "arith.cpp"
-  #include "blas.cpp"
-  #include "lapack.cpp"
 }
 extern "C" {
   #include "arrayfire.c"
