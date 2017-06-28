b1d7a69af272d3e842cd06825441608658182f34
Prasun Anand <prasunanand.bitsp@gmail.com>
2017-06-29 00:12:17 +0530

    arith: add unary operator definitions

--- ext/mri/arrayfire.c
+++ ext/mri/arrayfire.c
@@ -180,6 +180,16 @@ static VALUE arf_ew_##name(VALUE left_val, VALUE right_val) {              \
   return Data_Wrap_Struct(CLASS_OF(left_val), NULL, arf_free, result);     \
 }
 
+#define DEF_UNARY_RUBY_ACCESSOR(oper, name)                                \
+static VALUE arf_unary_##name(VALUE self) {                                \
+  afstruct* obj;                                                           \
+  afstruct* result = ALLOC(afstruct);                                      \
+  Data_Get_Struct(self, afstruct, obj);                                    \
+  af_##oper(&result->carray, obj->carray);                                 \
+  af_print_array(result->carray);                                          \
+  return Data_Wrap_Struct(CLASS_OF(self), NULL, arf_free, result);         \
+}
+
 #define DECL_ELEMENTWISE_RUBY_ACCESSOR(name)    static VALUE arf_ew_##name(VALUE left_val, VALUE right_val);
 #define DECL_UNARY_RUBY_ACCESSOR(name)          static VALUE arf_unary_##name(VALUE self);
 
@@ -479,6 +489,31 @@ DEF_ELEMENTWISE_RUBY_ACCESSOR(add, add)
 DEF_ELEMENTWISE_RUBY_ACCESSOR(subtract, sub)
 DEF_ELEMENTWISE_RUBY_ACCESSOR(multiply, mul)
 DEF_ELEMENTWISE_RUBY_ACCESSOR(divide, div)
+
+DEF_UNARY_RUBY_ACCESSOR(sin, sin)
+DEF_UNARY_RUBY_ACCESSOR(cos, cos)
+DEF_UNARY_RUBY_ACCESSOR(tan, tan)
+DEF_UNARY_RUBY_ACCESSOR(asin, asin)
+DEF_UNARY_RUBY_ACCESSOR(acos, acos)
+DEF_UNARY_RUBY_ACCESSOR(atan, atan)
+DEF_UNARY_RUBY_ACCESSOR(sinh, sinh)
+DEF_UNARY_RUBY_ACCESSOR(cosh, cosh)
+DEF_UNARY_RUBY_ACCESSOR(tanh, tanh)
+DEF_UNARY_RUBY_ACCESSOR(asinh, asinh)
+DEF_UNARY_RUBY_ACCESSOR(acosh, acosh)
+DEF_UNARY_RUBY_ACCESSOR(atanh, atanh)
+DEF_UNARY_RUBY_ACCESSOR(exp, exp)
+DEF_UNARY_RUBY_ACCESSOR(log2, log2)
+DEF_UNARY_RUBY_ACCESSOR(log1p, log1p)
+DEF_UNARY_RUBY_ACCESSOR(log10, log10)
+DEF_UNARY_RUBY_ACCESSOR(sqrt, sqrt)
+DEF_UNARY_RUBY_ACCESSOR(erf, erf)
+DEF_UNARY_RUBY_ACCESSOR(erfc, erfc)
+DEF_UNARY_RUBY_ACCESSOR(cbrt, cbrt)
+DEF_UNARY_RUBY_ACCESSOR(lgamma, lgamma)
+DEF_UNARY_RUBY_ACCESSOR(tgamma, tgamma)
+DEF_UNARY_RUBY_ACCESSOR(floor, floor)
+DEF_UNARY_RUBY_ACCESSOR(ceil, ceil)
 
 // Algorithm
 
