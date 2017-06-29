69505240cfd124eec647c3cc2fb84f812f7ff4d4
Prasun Anand <prasunanand.bitsp@gmail.com>
2017-06-29 10:43:41 +0530

    move cuda functions to cuda.c

---
+++ ext/mri/cmodules/cuda.c
@@ -0,0 +1,11 @@
+static VALUE arf_get_stream(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_get_native_id(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_set_native_id(VALUE self){
+  return Qnil;
+}
