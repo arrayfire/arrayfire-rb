1b49497eaa22991350599d68dd186db77583808e
Prasun Anand <prasunanand.bitsp@gmail.com>
2017-06-29 10:40:05 +0530

    move opencl functions to opencl.c

---
+++ ext/mri/cmodules/opencl.c
@@ -0,0 +1,35 @@
+static VALUE arf_get_context(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_get_queue(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_get_device_id(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_set_device_id(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_add_device_context(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_set_device_context(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_delete_device_context(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_get_device_type(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_get_platform(VALUE self){
+  return Qnil;
+}
