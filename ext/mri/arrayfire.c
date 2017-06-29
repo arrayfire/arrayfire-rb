6889cb833d382348d311986cbcc6de8ac651203a
Prasun Anand <prasunanand.bitsp@gmail.com>
2017-06-29 10:40:05 +0530

    move opencl functions to opencl.c

--- ext/mri/arrayfire.c
+++ ext/mri/arrayfire.c
@@ -653,45 +653,7 @@ static VALUE arf_get_device_ptr(VALUE self){
   return Qnil;
 }
 
-// OpenCL
-
-
-static VALUE arf_get_context(VALUE self){
-  return Qnil;
-}
-
-static VALUE arf_get_queue(VALUE self){
-  return Qnil;
-}
-
-static VALUE arf_get_device_id(VALUE self){
-  return Qnil;
-}
-
-static VALUE arf_set_device_id(VALUE self){
-  return Qnil;
-}
-
-static VALUE arf_add_device_context(VALUE self){
-  return Qnil;
-}
-
-static VALUE arf_set_device_context(VALUE self){
-  return Qnil;
-}
-
-static VALUE arf_delete_device_context(VALUE self){
-  return Qnil;
-}
-
-static VALUE arf_get_device_type(VALUE self){
-  return Qnil;
-}
-
-static VALUE arf_get_platform(VALUE self){
-  return Qnil;
-}
-
+#include "cmodules/opencl.c"
 #include "cmodules/data.c"
 #include "cmodules/lapack.c"
 #include "cmodules/random.c"
