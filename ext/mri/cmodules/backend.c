d941e7db33649f27e132ab96a77ae0e560d5a786
Prasun Anand <prasunanand.bitsp@gmail.com>
2017-06-29 10:45:51 +0530

    move backend functions to backend.c

---
+++ ext/mri/cmodules/backend.c
@@ -0,0 +1,19 @@
+static VALUE arf_get_backend_count(VALUE self){
+   return Qnil;
+}
+
+static VALUE arf_get_available_backends(VALUE self){
+   return Qnil;
+}
+
+static VALUE arf_get_backend_id(VALUE self){
+   return Qnil;
+}
+
+static VALUE arf_get_active_backend(VALUE self){
+   return Qnil;
+}
+
+static VALUE arf_get_backend_device_id(VALUE self){
+   return Qnil;
+}
