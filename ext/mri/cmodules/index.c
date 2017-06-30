b6a360cbd5461072cd8b81a5f2231036262b4be5
Prasun Anand <prasunanand.bitsp@gmail.com>
2017-06-30 14:45:07 +0530

    add bindings for Index module

---
+++ ext/mri/cmodules/index.c
@@ -0,0 +1,39 @@
+static VALUE arf_index(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_lookup(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_assign_seq(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_index_gen(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_assign_gen(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_create_indexers(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_set_array_indexer(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_set_seq_indexer(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_set_seq_param_indexer(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_release_indexers(VALUE self){
+  return Qnil;
+}
