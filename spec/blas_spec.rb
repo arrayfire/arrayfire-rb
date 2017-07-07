3f40cb69e508c9d3f824645e9efbd24950166942
Prasun Anand <prasunanand.bitsp@gmail.com>
2017-07-07 12:53:55 +0530

    modify blas spec

--- spec/blas_spec.rb
+++ spec/blas_spec.rb
@@ -5,7 +5,7 @@ describe ArrayFire::BLAS do
     let(:a) { ArrayFire::Af_Array.new 2, [2,2],[1,2,3,4] }
     let(:b) { ArrayFire::Af_Array.new 2, [2,2],[1,2,3,4] }
     let(:c) { ArrayFire::Af_Array.new 2, [2,2],[7.0,10.0,15.0,22.0] }
-    subject {c}
-    it {expect(ArrayFire::BLAS.matmul(a,b).array).to eq c.array}
+    subject {ArrayFire::BLAS.matmul(a,b)}
+    it {is_expected.to eq c}
   end
 end
