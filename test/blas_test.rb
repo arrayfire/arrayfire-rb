26be3d43cd89f109ee628a6fd3d02e607bd2f0c1
prasun <prasunanand.bitsp@gmail.com>
2017-07-24 00:28:17 +0530

    added blas tests

--- test/blas_test.rb
+++ test/blas_test.rb
@@ -3,13 +3,25 @@ require 'test_helper'
 class ArrayFire::BLASTest < Minitest::Test
 
   def setup
-    @a = ArrayFire::Af_Array.new 2, [2,2],[1,2,3,4]
-    @b = ArrayFire::Af_Array.new 2, [2,2],[1,2,3,4]
+    @matrix_left  = ArrayFire::Af_Array.new 2, [2,2],[ 12, 21,-61, 48]
+    @matrix_right = ArrayFire::Af_Array.new 2, [2,2],[-15, 41, 30 , 7 ]
+    @vector_left  = ArrayFire::Af_Array.new 2, [4,1],[-15, 41, 30 , 7 ]
+    @vector_right = ArrayFire::Af_Array.new 2, [4,1],[ 25, 141, 17 , 7 ]
   end
 
   def test_matmul
-    c =  ArrayFire::Af_Array.new 2, [2,2],[7.0,10.0,15.0,22.0]
-    assert_equal c, ArrayFire::BLAS.matmul(@a,@b)
+    result = ArrayFire::Af_Array.new 2, [2,2],[-2681.0, 1653.0, -67.0, 966.0]
+    assert_equal result, ArrayFire::BLAS.matmul(@matrix_left, @matrix_right)
+  end
+
+  def test_dot
+    result = ArrayFire::Af_Array.new 1,[1],[5965.0]
+    assert_equal result, ArrayFire::BLAS.dot(@vector_left, @vector_right)
+  end
+
+  def test_transpose
+    result = ArrayFire::Af_Array.new 2, [2,2], [12.0, -61.0, 21.0, 48.0]
+    assert_equal result, ArrayFire::BLAS.transpose(@matrix_left)
   end
 
 end
