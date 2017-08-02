1140da3af3b8ece8a781565c54fea1da6b1fccb0
prasun <prasunanand.bitsp@gmail.com>
2017-08-02 11:36:09 +0530

    init ArrayFire::Util class

---
+++ ext/mri/cmodules/util.c
@@ -0,0 +1,33 @@
+static VALUE arf_print_array(VALUE self){
+  return Qnil;
+}
+static VALUE arf_print_array_gen(VALUE self){
+  return Qnil;
+}
+static VALUE arf_save_array(VALUE self){
+  return Qnil;
+}
+static VALUE arf_read_array_index(VALUE self){
+  return Qnil;
+}
+static VALUE arf_read_array_key(VALUE self){
+  return Qnil;
+}
+static VALUE arf_read_array_key_check(VALUE self){
+  return Qnil;
+}
+static VALUE arf_array_to_string(VALUE self){
+  return Qnil;
+}
+static VALUE arf_example_function(VALUE self){
+  return Qnil;
+}
+static VALUE arf_get_version(VALUE self){
+  return Qnil;
+}
+static VALUE arf_get_revision(VALUE self){
+  return Qnil;
+}
+static VALUE arf_get_size_of(VALUE self){
+  return Qnil;
+}
