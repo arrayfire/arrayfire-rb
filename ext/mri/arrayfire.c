1a4f5f16a645a6c2d4d059398e4e69490ba2752c
prasun <prasunanand.bitsp@gmail.com>
2017-08-08 17:14:18 +0530

    move arith functions to arith.c

--- ext/mri/arrayfire.c
+++ ext/mri/arrayfire.c
@@ -280,28 +280,6 @@ static VALUE arf_get_version(VALUE self);
 static VALUE arf_get_revision(VALUE self);
 static VALUE arf_get_size_of(VALUE self);
 
-#define DEF_ELEMENTWISE_RUBY_ACCESSOR(name, oper)                          \
-static VALUE arf_ew_##name(VALUE left_val, VALUE right_val) {              \
-  afstruct* left;                                                          \
-  afstruct* right;                                                         \
-  afstruct* result = ALLOC(afstruct);                                      \
-  Data_Get_Struct(left_val, afstruct, left);                               \
-  Data_Get_Struct(right_val, afstruct, right);                             \
-  af_##oper(&result->carray,  left->carray, right->carray, true);          \
-  af_print_array(result->carray);                                          \
-  return Data_Wrap_Struct(CLASS_OF(left_val), NULL, arf_free, result);     \
-}
-
-#define DEF_UNARY_RUBY_ACCESSOR(oper, name)                                \
-static VALUE arf_unary_##name(VALUE self) {                                \
-  afstruct* obj;                                                           \
-  afstruct* result = ALLOC(afstruct);                                      \
-  Data_Get_Struct(self, afstruct, obj);                                    \
-  af_##oper(&result->carray, obj->carray);                                 \
-  af_print_array(result->carray);                                          \
-  return Data_Wrap_Struct(CLASS_OF(self), NULL, arf_free, result);         \
-}
-
 #define DECL_ELEMENTWISE_RUBY_ACCESSOR(name)    static VALUE arf_ew_##name(VALUE left_val, VALUE right_val);
 #define DECL_UNARY_RUBY_ACCESSOR(name)          static VALUE arf_unary_##name(VALUE self);
 
@@ -758,45 +736,9 @@ static VALUE arf_eqeq_approx(VALUE left_val, VALUE right_val) {
   return Qtrue;
 }
 
-DEF_ELEMENTWISE_RUBY_ACCESSOR(add, add)
-DEF_ELEMENTWISE_RUBY_ACCESSOR(subtract, sub)
-DEF_ELEMENTWISE_RUBY_ACCESSOR(multiply, mul)
-DEF_ELEMENTWISE_RUBY_ACCESSOR(divide, div)
-
-DEF_ELEMENTWISE_RUBY_ACCESSOR(eqeq, eq)
-DEF_ELEMENTWISE_RUBY_ACCESSOR(neq, neq)
-DEF_ELEMENTWISE_RUBY_ACCESSOR(leq, le)
-DEF_ELEMENTWISE_RUBY_ACCESSOR(geq, ge)
-DEF_ELEMENTWISE_RUBY_ACCESSOR(lt, lt)
-DEF_ELEMENTWISE_RUBY_ACCESSOR(gt, gt)
-
-DEF_UNARY_RUBY_ACCESSOR(sin, sin)
-DEF_UNARY_RUBY_ACCESSOR(cos, cos)
-DEF_UNARY_RUBY_ACCESSOR(tan, tan)
-DEF_UNARY_RUBY_ACCESSOR(asin, asin)
-DEF_UNARY_RUBY_ACCESSOR(acos, acos)
-DEF_UNARY_RUBY_ACCESSOR(atan, atan)
-DEF_UNARY_RUBY_ACCESSOR(sinh, sinh)
-DEF_UNARY_RUBY_ACCESSOR(cosh, cosh)
-DEF_UNARY_RUBY_ACCESSOR(tanh, tanh)
-DEF_UNARY_RUBY_ACCESSOR(asinh, asinh)
-DEF_UNARY_RUBY_ACCESSOR(acosh, acosh)
-DEF_UNARY_RUBY_ACCESSOR(atanh, atanh)
-DEF_UNARY_RUBY_ACCESSOR(exp, exp)
-DEF_UNARY_RUBY_ACCESSOR(log2, log2)
-DEF_UNARY_RUBY_ACCESSOR(log1p, log1p)
-DEF_UNARY_RUBY_ACCESSOR(log10, log10)
-DEF_UNARY_RUBY_ACCESSOR(sqrt, sqrt)
-DEF_UNARY_RUBY_ACCESSOR(erf, erf)
-DEF_UNARY_RUBY_ACCESSOR(erfc, erfc)
-DEF_UNARY_RUBY_ACCESSOR(cbrt, cbrt)
-DEF_UNARY_RUBY_ACCESSOR(lgamma, lgamma)
-DEF_UNARY_RUBY_ACCESSOR(tgamma, tgamma)
-DEF_UNARY_RUBY_ACCESSOR(floor, floor)
-DEF_UNARY_RUBY_ACCESSOR(ceil, ceil)
-
 #include "cmodules/array.c"
 #include "cmodules/algorithm.c"
+#include "cmodules/arith.c"
 #include "cmodules/backend.c"
 #include "cmodules/blas.c"
 #include "cmodules/cuda.c"
--- ext/mri/cmodules/arith.c
+++ ext/mri/cmodules/arith.c
@@ -1,7 +1,58 @@
-static void add(afstruct *result, afstruct *left, afstruct *right)
-{
-  array l = array(left->dimension[0], left->dimension[1], left->array);
-  array r = array(right->dimension[0], right->dimension[1], right->array);
-  array res = operator+(l,r);
-  result->array = res.host<double>();
+#define DEF_ELEMENTWISE_RUBY_ACCESSOR(name, oper)                          \
+static VALUE arf_ew_##name(VALUE left_val, VALUE right_val) {              \
+  afstruct* left;                                                          \
+  afstruct* right;                                                         \
+  afstruct* result = ALLOC(afstruct);                                      \
+  Data_Get_Struct(left_val, afstruct, left);                               \
+  Data_Get_Struct(right_val, afstruct, right);                             \
+  af_##oper(&result->carray,  left->carray, right->carray, true);          \
+  af_print_array(result->carray);                                          \
+  return Data_Wrap_Struct(CLASS_OF(left_val), NULL, arf_free, result);     \
 }
+
+#define DEF_UNARY_RUBY_ACCESSOR(oper, name)                                \
+static VALUE arf_unary_##name(VALUE self) {                                \
+  afstruct* obj;                                                           \
+  afstruct* result = ALLOC(afstruct);                                      \
+  Data_Get_Struct(self, afstruct, obj);                                    \
+  af_##oper(&result->carray, obj->carray);                                 \
+  af_print_array(result->carray);                                          \
+  return Data_Wrap_Struct(CLASS_OF(self), NULL, arf_free, result);         \
+}
+
+DEF_ELEMENTWISE_RUBY_ACCESSOR(add, add)
+DEF_ELEMENTWISE_RUBY_ACCESSOR(subtract, sub)
+DEF_ELEMENTWISE_RUBY_ACCESSOR(multiply, mul)
+DEF_ELEMENTWISE_RUBY_ACCESSOR(divide, div)
+
+DEF_ELEMENTWISE_RUBY_ACCESSOR(eqeq, eq)
+DEF_ELEMENTWISE_RUBY_ACCESSOR(neq, neq)
+DEF_ELEMENTWISE_RUBY_ACCESSOR(leq, le)
+DEF_ELEMENTWISE_RUBY_ACCESSOR(geq, ge)
+DEF_ELEMENTWISE_RUBY_ACCESSOR(lt, lt)
+DEF_ELEMENTWISE_RUBY_ACCESSOR(gt, gt)
+
+DEF_UNARY_RUBY_ACCESSOR(sin, sin)
+DEF_UNARY_RUBY_ACCESSOR(cos, cos)
+DEF_UNARY_RUBY_ACCESSOR(tan, tan)
+DEF_UNARY_RUBY_ACCESSOR(asin, asin)
+DEF_UNARY_RUBY_ACCESSOR(acos, acos)
+DEF_UNARY_RUBY_ACCESSOR(atan, atan)
+DEF_UNARY_RUBY_ACCESSOR(sinh, sinh)
+DEF_UNARY_RUBY_ACCESSOR(cosh, cosh)
+DEF_UNARY_RUBY_ACCESSOR(tanh, tanh)
+DEF_UNARY_RUBY_ACCESSOR(asinh, asinh)
+DEF_UNARY_RUBY_ACCESSOR(acosh, acosh)
+DEF_UNARY_RUBY_ACCESSOR(atanh, atanh)
+DEF_UNARY_RUBY_ACCESSOR(exp, exp)
+DEF_UNARY_RUBY_ACCESSOR(log2, log2)
+DEF_UNARY_RUBY_ACCESSOR(log1p, log1p)
+DEF_UNARY_RUBY_ACCESSOR(log10, log10)
+DEF_UNARY_RUBY_ACCESSOR(sqrt, sqrt)
+DEF_UNARY_RUBY_ACCESSOR(erf, erf)
+DEF_UNARY_RUBY_ACCESSOR(erfc, erfc)
+DEF_UNARY_RUBY_ACCESSOR(cbrt, cbrt)
+DEF_UNARY_RUBY_ACCESSOR(lgamma, lgamma)
+DEF_UNARY_RUBY_ACCESSOR(tgamma, tgamma)
+DEF_UNARY_RUBY_ACCESSOR(floor, floor)
+DEF_UNARY_RUBY_ACCESSOR(ceil, ceil)
