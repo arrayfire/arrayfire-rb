00f68e5081b722f45424762f50525cdc948a498b
prasun <prasunanand.bitsp@gmail.com>
2017-08-08 12:29:10 +0530

    ArrayFire::Backend bindings

--- ext/mri/cmodules/backend.c
+++ ext/mri/cmodules/backend.c
@@ -4,18 +4,32 @@ static VALUE arf_get_backend_count(VALUE self){
   return UINT2NUM(num_backends);
 }
 
+// int backends = af::getAvailableBackends();
+// bool cpu    = backends & AF_BACKEND_CPU;
+// bool cuda   = backends & AF_BACKEND_CUDA;
+// bool opencl = backends & AF_BACKEND_OPENCL;
+
 static VALUE arf_get_available_backends(VALUE self){
   int backends;
-  af_get_available_backends (&backends);
+  af_get_available_backends(&backends);
   return INT2NUM(backends);
 }
 
 static VALUE arf_get_backend_id(VALUE self, VALUE array_val){
-  return Qnil;
+  afstruct* input;
+  Data_Get_Struct(array_val, afstruct, input);
+  af_backend backend;
+  af_get_backend_id (&backend, input->carray);
+
+  const char* backend_name = get_backend_name(backend);
+  return rb_str_new_cstr(backend_name);
 }
 
 static VALUE arf_get_active_backend(VALUE self){
-  return Qnil;
+  af_backend backend;
+  af_get_active_backend(&backend);
+  const char* backend_name = get_backend_name(backend);
+  return rb_str_new_cstr(backend_name);
 }
 
 static VALUE arf_get_backend_device_id(VALUE self, VALUE array_val){
@@ -29,6 +43,8 @@ static VALUE arf_get_backend_device_id(VALUE self, VALUE array_val){
   return INT2NUM(device_id);
 }
 
-static VALUE arf_set_backend(VALUE self){
+static VALUE arf_set_backend(VALUE self, VALUE backend_val){
+  af_backend backend = arf_backend_type_from_rbsymbol(backend_val);
+  af_set_backend(backend);
   return Qnil;
 }
