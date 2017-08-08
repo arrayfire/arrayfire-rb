296e61e93c1117a806873016c2146b7964384e03
prasun <prasunanand.bitsp@gmail.com>
2017-08-08 14:24:50 +0530

    added opencl bindings

--- ext/mri/cmodules/opencl.c
+++ ext/mri/cmodules/opencl.c
@@ -1,3 +1,24 @@
+std::map<char*, size_t> AFCL_DEVICE_TYPES = {
+  {"AFCL_DEVICE_TYPE_CPU",       0},
+  {"CL_DEVICE_TYPE_CPU",         1},
+  {"AFCL_DEVICE_TYPE_GPU",       2},
+  {"CL_DEVICE_TYPE_GPU",         3},
+  {"AFCL_DEVICE_TYPE_ACC",       4},
+  {"CL_DEVICE_TYPE_ACCELERATOR", 5},
+  {"AFCL_DEVICE_TYPE_UNKNOWN",  -1}
+};
+
+std::map<char*, size_t> AFCL_PLATFORM_TYPES = {
+  {"AFCL_PLATFORM_AMD",      0},
+  {"AFCL_PLATFORM_APPLE",    1},
+  {"AFCL_PLATFORM_INTEL",    2},
+  {"AFCL_PLATFORM_NVIDIA",   3},
+  {"AFCL_PLATFORM_BEIGNET",  4},
+  {"AFCL_PLATFORM_POCL",     5},
+  {"AFCL_PLATFORM_UNKNOWN", -1}
+};
+
+
 static VALUE arf_get_context(VALUE self){
   return Qnil;
 }
@@ -7,6 +28,8 @@ static VALUE arf_get_queue(VALUE self){
 }
 
 static VALUE arf_get_device_id(VALUE self){
+  cl_device_id id;
+  afcl_get_device_id(&id);
   return Qnil;
 }
 
@@ -27,9 +50,31 @@ static VALUE arf_delete_device_context(VALUE self){
 }
 
 static VALUE arf_get_device_type(VALUE self){
-  return Qnil;
+  afcl_device_type device;
+  afcl_get_device_type(&device);
+  return rb_str_new_cstr(get_cl_device_name(device));
 }
 
 static VALUE arf_get_platform(VALUE self){
-  return Qnil;
+  afcl_platform platform;
+  afcl_get_platform(&platform);
+  return rb_str_new_cstr(get_cl_platform_name(platform));
+}
+
+const char* get_cl_device_name(afcl_device_type device){
+  for(std::map<char*, size_t>::value_type& entry : AFCL_DEVICE_TYPES) {
+    if (device == entry.second) {
+      return entry.first;
+    }
+  }
+  rb_raise(rb_eArgError, "Couldn't detect device!");
+}
+
+const char* get_cl_platform_name(afcl_platform platform){
+  for(std::map<char*, size_t>::value_type& entry : AFCL_PLATFORM_TYPES) {
+    if (platform == entry.second) {
+      return entry.first;
+    }
+  }
+  rb_raise(rb_eArgError, "Couldn't detect platform!");
 }
