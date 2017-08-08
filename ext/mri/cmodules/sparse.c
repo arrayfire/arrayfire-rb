ebfd3195519cb87c96fda0e28bf3e3448e418294
prasun <prasunanand.bitsp@gmail.com>
2017-08-08 17:08:27 +0530

    ArrayFire::Sparse class init

---
+++ ext/mri/cmodules/sparse.c
@@ -0,0 +1,43 @@
+static VALUE arf_create_sparse_array(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_create_sparse_array_from_ptr(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_create_sparse_array_from_dense(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_sparse_convert_to(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_sparse_to_dense(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_sparse_get_info(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_sparse_get_values(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_sparse_get_row_idx(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_sparse_get_col_idx(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_sparse_get_nnz(VALUE self){
+  return Qnil;
+}
+
+static VALUE arf_sparse_get_storage(VALUE self){
+  return Qnil;
+}
